3311e5f82dd3a0a8137173e7a6aeaf8c9cc2ed4e
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-13 21:50:25 -0400

    initial commit

---
+++ src/main/java/soe/mdeis/m7/solid/App.java
@@ -0,0 +1,13 @@
+package soe.mdeis.m7.solid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/soe/mdeis/m7/solid/AppTests.java
@@ -0,0 +1,13 @@
+package soe.mdeis.m7.solid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
