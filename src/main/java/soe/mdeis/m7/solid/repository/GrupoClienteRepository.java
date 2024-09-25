129cc42ed2d558df3644b593ece3bcf18cc081a2
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-25 19:53:43 -0400

    GrupoCliente repository Added

---
+++ src/main/java/soe/mdeis/m7/solid/repository/GrupoClienteRepository.java
@@ -0,0 +1,9 @@
+package soe.mdeis.m7.solid.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import soe.mdeis.m7.solid.model.GrupoCliente;
+
+public interface GrupoClienteRepository extends JpaRepository<GrupoCliente, Integer> {
+
+}
