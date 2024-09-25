bf3d3582413067006f3976b7e93d01962dd77f64
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-25 18:36:39 -0400

    Almacen Repository Added

---
+++ src/main/java/soe/mdeis/m7/solid/repository/AlmacenRepository.java
@@ -0,0 +1,9 @@
+package soe.mdeis.m7.solid.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import soe.mdeis.m7.solid.model.Almacen;
+
+public interface AlmacenRepository extends JpaRepository<Almacen, Integer> {
+
+}
