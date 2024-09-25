7cba05e395535f3d1a902e110ae768335baa199c
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-25 16:36:07 -0400

    Cliente Repository Added

---
+++ src/main/java/soe/mdeis/m7/solid/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package soe.mdeis.m7.solid.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import soe.mdeis.m7.solid.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
