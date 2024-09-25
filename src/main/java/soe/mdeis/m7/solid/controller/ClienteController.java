1a8ed98d070f16e8d3741c15166ca32f03811163
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-25 17:18:54 -0400

    Cliente Controller getMapping added

--- src/main/java/soe/mdeis/m7/solid/controller/ClienteController.java
+++ src/main/java/soe/mdeis/m7/solid/controller/ClienteController.java
@@ -6,11 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import soe.mdeis.m7.solid.model.Cliente;
 import soe.mdeis.m7.solid.service.ClienteService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("api")
@@ -22,6 +26,11 @@ public class ClienteController {
    @PostMapping("/clientes")
    public ResponseEntity<?> postMethodName(@RequestBody Cliente cliente) {
       return new ResponseEntity<Cliente>(clienteService.save(cliente), HttpStatus.CREATED);
+   }
+
+   @GetMapping("/clientes")
+   public ResponseEntity<List<Cliente>> getMethodName() {
+      return new ResponseEntity<List<Cliente>>(clienteService.getAll(), HttpStatus.OK);
    }
 
 }
