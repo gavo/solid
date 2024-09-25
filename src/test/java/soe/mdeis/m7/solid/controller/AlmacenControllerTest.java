68faee0141b750590129f6bc09d46f7b48d6508d
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-25 18:36:02 -0400

    Added Almacen Controller Test

---
+++ src/test/java/soe/mdeis/m7/solid/controller/AlmacenControllerTest.java
@@ -0,0 +1,66 @@
+package soe.mdeis.m7.solid.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import soe.mdeis.m7.solid.model.Almacen;
+import soe.mdeis.m7.solid.service.AlmacenService;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@WebMvcTest(AlmacenController.class)
+@AutoConfigureMockMvc
+@DisplayName("Test Almacen Controller")
+public class AlmacenControllerTest {
+   @Autowired
+   private MockMvc mockMvc;
+
+   @MockBean
+   private AlmacenService almacenService;
+
+   @Test
+   @DisplayName("Save new Almacen")
+   void shouldSaveANewAlmacen() throws Exception {
+      Almacen newAlmacen = new Almacen(1, "Almacen 1", "Alm-01", true);
+
+      Mockito.when(almacenService.save(Mockito.any(Almacen.class))).thenReturn(newAlmacen);
+
+      mockMvc.perform(post("/api/almacen")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"nombreAlmacen\":\"Almacen 1\",\"denominativo\":\"Alm-01\",\"estado\":\"true\"}"))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.idAlmacen", is(1)))
+            .andExpect(jsonPath("$.nombreAlmacen", is("Almacen 1")));
+   }
+
+   @Test
+   @DisplayName("Get Almacenes")
+   void showListAllAlmacenes() throws Exception {
+      List<Almacen> Almacenes = new ArrayList<Almacen>(Arrays.asList(
+            new Almacen(1, "Almacen 1", "Al-1", true),
+            new Almacen(2, "Almacen 2", "Al-02", true)));
+
+      Mockito.when(almacenService.getAll()).thenReturn(Almacenes);
+
+      mockMvc.perform(get("/api/almacen")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(2)));
+   }
+}
