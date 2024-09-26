39697784be47a10d72d072a6f3af2b8924eb7ce1
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-26 13:14:25 -0400

    ServicioRepository added

---
+++ src/main/java/soe/mdeis/m7/solid/repository/ServicioRepository.java
@@ -0,0 +1,9 @@
+package soe.mdeis.m7.solid.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import soe.mdeis.m7.solid.model.Servicio;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Integer> {
+
+}
