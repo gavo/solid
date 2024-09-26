c902a1a72e4c593f2e2e28f78abefe03ac3e7f11
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-26 13:14:37 -0400

    Servicio model added

---
+++ src/main/java/soe/mdeis/m7/solid/model/Servicio.java
@@ -0,0 +1,26 @@
+package soe.mdeis.m7.solid.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "servicio")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Servicio {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   int id;
+
+   String codigo;
+
+   String nombre;
+}
