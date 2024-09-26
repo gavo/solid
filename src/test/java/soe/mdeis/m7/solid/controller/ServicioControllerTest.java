a556dd5ca2a956955a8c4b9a248fc0ccc2516994
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-26 13:13:46 -0400

    ServicioControllerTest added

---
+++ src/test/java/soe/mdeis/m7/solid/controller/ServicioControllerTest.java
@@ -0,0 +1,68 @@
+package soe.mdeis.m7.solid.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import soe.mdeis.m7.solid.model.Servicio;
+import soe.mdeis.m7.solid.service.ServicioService;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@WebMvcTest(ServicioController.class)
+@AutoConfigureMockMvc
+@DisplayName("Test Servicio Controller")
+public class ServicioControllerTest {
+
+   @Autowired
+   MockMvc mockMvc;
+
+   @MockBean
+   ServicioService service;
+
+   @Test
+   @DisplayName("Save new Servicio")
+   void shouldSaveANewServicio() throws Exception {
+      Servicio newServicio = new Servicio(1, "S-01", "Servicio 1");
+
+      Mockito.when(service.save(Mockito.any(Servicio.class))).thenReturn(newServicio);
+
+      mockMvc.perform(post("/api/servicio")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"codigo\":\"S-01\",\"nombre\":\"Servicio 1\"}"))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.nombre", is("Servicio 1")));
+   }
+
+   @Test
+   @DisplayName("Get Servicios")
+   void showListAllAlmacenes() throws Exception {
+      List<Servicio> list = new ArrayList<Servicio>(Arrays.asList(
+            new Servicio(1, "ser-01", "Servicio 1"),
+            new Servicio(1, "ser-02", "Servicio 2"),
+            new Servicio(1, "ser-03", "Servicio 3")));
+
+      Mockito.when(service.getAll()).thenReturn(list);
+
+      mockMvc.perform(get("/api/servicio")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(3)));
+   }
+}
