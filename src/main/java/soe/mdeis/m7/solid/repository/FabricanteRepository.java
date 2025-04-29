a5513c9fb12e4e8276beb9574d77695d145c51ab
Gustavo Vargas Miranda <gvmgust@gmail.com>
2025-04-28 23:40:45 -0400

    FakeFabriante endpoint created

--- src/main/java/soe/mdeis/m7/solid/repository/FabricanteRepository.java
+++ src/main/java/soe/mdeis/m7/solid/repository/FabricanteRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import soe.mdeis.m7.solid.model.Fabricante;
 
+import java.util.Optional;
 
 public interface FabricanteRepository extends JpaRepository<Fabricante, Long> {
 
+    Optional<Fabricante> findByNombre(String nombre);
 
 }
